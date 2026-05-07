b704ffda171f77f087cf070acc41c490df3db80f
Simon Rappenecker <simi2657@gmail.com>
2026-05-07 10:36:40 +0200

    Completed CV notebook

--- packages/visual_lane_servoing/include/visual_servoing_solution.py
+++ packages/visual_lane_servoing/include/visual_servoing_solution.py
@@ -30,7 +30,7 @@ def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
     if max_val != 0:
         steer_unit /= max_val
 
-    steer_matrix_left_lane[:, :width] = 1 # CHANGE ME
+    steer_matrix_left_lane[:, :width] = steer_unit
 
     return steer_matrix_left_lane
 
@@ -59,7 +59,7 @@ def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
     if max_val != 0:
         steer_unit /= max_val
 
-    steer_matrix_right_lane[:, width:] = 1 # CHANGE ME
+    steer_matrix_right_lane[:, width:] = steer_unit
 
     return steer_matrix_right_lane
 
@@ -73,12 +73,12 @@ def detect_lane_markings(image: np.ndarray, projector: GroundProjector) -> Tuple
         right_masked_img:  Masked image for the solid-white line (numpy.ndarray)
     """
 
-    sigma = 8  # CHANGE ME - Gaussian blur sigma
-    threshold = 10  # CHANGE ME - minimum threshold for gradient magnitude
-    white_lower_hsv = np.array([0, 0, 0])  # CHANGE ME - color thresholds
-    white_upper_hsv = np.array([179, 255, 255])  # CHANGE ME
-    yellow_lower_hsv = np.array([0, 0, 0])  # CHANGE ME
-    yellow_upper_hsv = np.array([179, 255, 255])  # CHANGE ME
+    sigma = 2
+    threshold = 50
+    white_lower_hsv = np.array([0, 12, 132])
+    white_upper_hsv = np.array([180, 80, 255])
+    yellow_lower_hsv = np.array([14, 75, 100])   
+    yellow_upper_hsv = np.array([180, 255, 255])
 
     h, w, _ = image.shape
 
